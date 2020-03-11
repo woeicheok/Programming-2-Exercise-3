89217bdafd92c0ff0dbc9b4385728e79cd0c6b3a
Eric Tan Wei Cheok <61194399+woeicheok@users.noreply.github.com>
2020-03-11 18:39:03 +0800

    Add files via upload

---
+++ StringClass.java
@@ -0,0 +1,8 @@
+public class StringClass
+{
+   public static void main(String[]args)
+   {
+    String object1 = "Hello how are you";
+    String object2 = new String("Hello, I am fine");
+    }
+}
