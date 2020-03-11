af43f6d7eaff4041139249d91cd6ed6636b28e1c
Eric Tan Wei Cheok <61194399+woeicheok@users.noreply.github.com>
2020-03-11 18:40:03 +0800

    Add files via upload

---
+++ Question3.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+public class Question3
+{
+   public static void main(String[]args)
+   {
+
+   Random rd = new Random();
+   System.out.println(rd.nextInt(41) + 10);
+   }
+}// the output will be random pick from the number to 40 and add 10 to the output
