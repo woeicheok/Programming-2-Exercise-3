f8b1c10669e4c1716a2d08b4d194bcba24ed40d5
Eric Tan Wei Cheok <61194399+woeicheok@users.noreply.github.com>
2020-03-11 18:39:27 +0800

    Add files via upload

---
+++ Question2.java
@@ -0,0 +1,9 @@
+public class Question2
+{
+   public static void main(String[]args)
+   {
+
+   String phr = new String("Big,blue sky");
+   System.out.println(phr.substring(phr.indexOf(","),7));
+   }
+}
